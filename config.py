9056e09622951900c7f0e1f6382beb02d1205a87
j0sh21 <132760416+j0sh21@users.noreply.github.com>
2023-10-03 22:36:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,18 +7,11 @@ os.chdir(script_dir)
 conf = configparser.ConfigParser()
 conf.read('cfg.ini')
 
-key = conf.get('API', 'key')
-
 subject = conf.get('MAIL', 'subject')
 mailfrom = conf.get('MAIL', 'from')
 smtp = conf.get('MAIL', 'smtp')
 mail_usr = conf.get('MAIL', 'user')
 mail_pwd = conf.get('MAIL', 'pwd')
 mail_to = conf.get('MAIL', 'to')
-
-duser = conf.get('DATABASE', 'user')
-dpassword = conf.get('DATABASE', 'password')
-dhost = conf.get('DATABASE', 'host')
-ddatabase = conf.get('DATABASE', 'database')
 
 print('CFG erfolgreich geladen')
